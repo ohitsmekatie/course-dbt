f4dc957e3182491b85f9ba12dd9006dfdd145a5f
Katie Sipos <katie@planetscale.com>
2023-04-07 20:21:53 +0000

    week 1 setup

---
+++ greenery/models/staging/postgres/stg_orders.sql
@@ -0,0 +1,15 @@
+select  
+    order_id,
+    user_id,
+    promo_id, 
+    address_id, 
+    tracking_id,
+    created_at, 
+    estimated_delivery_at,
+    delivered_at, 
+    order_cost, 
+    shipping_cost,
+    order_total, 
+    shipping_service,
+    status
+from {{ source('postgres', 'orders') }}
